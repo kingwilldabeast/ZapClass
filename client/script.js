79de43f7b86ab06e4acdeeda217e56f253d3635d
kingwilldabeast <willmur@live.com>
2024-06-05 16:06:30 -0500

    expanded view replaces, not stacks

--- client/script.js
+++ client/script.js
@@ -88,14 +88,14 @@ async function refresh() {
         // console.log(`${comicObjectArray.length} comics are ${comicObjectArray}`)
         // console.log(`${venueObjectArray.length} venues are ${venueObjectArray}`)
 
-
+        //input form
         for (const comicObject of comicObjectArray) {
             const newHost = document.createElement('option');
             newHost.setAttribute('id',comicObject._id)
             newHost.value = comicObject.name
             hostOptions.appendChild(newHost)
         }
-
+        //input form
         for (const venueObject of venueObjectArray) {
             const newVenue = document.createElement('option');
             newVenue.setAttribute('id',venueObject._id)
@@ -109,13 +109,13 @@ async function refresh() {
             newCard.classList.add('show-card');
             newCard.setAttribute('id',eventObject._id)
     
-            if (eventObject.logo) {
-                const newImage = document.createElement('img');
-                newImage.classList.add('show-logo')
-                // console.log(eventObject.logo)
-                // newImage.setAttribute('src',eventObject.logo)
-                newCard.appendChild(newImage)
-            }
+            // if (eventObject.logo) {
+            //     const newImage = document.createElement('img');
+            //     newImage.classList.add('show-logo')
+            //     // console.log(eventObject.logo)
+            //     // newImage.setAttribute('src',eventObject.logo)
+            //     newCard.appendChild(newImage)
+            // }
     
             const newName = document.createElement('div');
             // newName.classList.add('show-name')
@@ -137,55 +137,55 @@ async function refresh() {
             newTime.innerText = eventObject.time
             newCard.appendChild(newTime)
 
-            const newWeekday = document.createElement('div');
-            // newWeekday.classList.add('show-location')
-            newWeekday.innerText = eventObject.weekday
-            newCard.appendChild(newWeekday)
+            // const newWeekday = document.createElement('div');
+            // // newWeekday.classList.add('show-location')
+            // newWeekday.innerText = eventObject.weekday
+            // newCard.appendChild(newWeekday)
 
-            const newFrequency = document.createElement('div');
-            // newFrequency.classList.add('show-frequency')
-            newFrequency.innerText = eventObject.frequency
-            newCard.appendChild(newFrequency)
+            // const newFrequency = document.createElement('div');
+            // // newFrequency.classList.add('show-frequency')
+            // newFrequency.innerText = eventObject.frequency
+            // newCard.appendChild(newFrequency)
 
-            if (eventObject.details) {
-                const newDetails = document.createElement('div');
-                // newDetails.classList.add('show-details')
-                newDetails.innerText = eventObject.details
-                newCard.appendChild(newDetails)
-            }
+            // if (eventObject.details) {
+            //     const newDetails = document.createElement('div');
+            //     // newDetails.classList.add('show-details')
+            //     newDetails.innerText = eventObject.details
+            //     newCard.appendChild(newDetails)
+            // }
 
-            if (eventObject.link) {
-                const newLink = document.createElement('div');
-                // newLink.classList.add('show-link')
-                newLink.innerText = eventObject.link
-                newCard.appendChild(newLink)
-            }
+            // if (eventObject.link) {
+            //     const newLink = document.createElement('div');
+            //     // newLink.classList.add('show-link')
+            //     newLink.innerText = eventObject.link
+            //     newCard.appendChild(newLink)
+            // }
 
-            if (eventObject.hosts) {
-                for (const hostObjectID of eventObject.hosts) {
-                    const newHost = document.createElement('div');
-                    newHost.classList.add('show-host')
-                    const comicObject = comicObjectArray.find(obj => obj._id.toString() === hostObjectID);
-                    newHost.innerText = `Host: ${comicObject.name}`
-                    newHost.addEventListener('click', function(event) {
-                    // console.log(hostObjectID)
-                    linkToComic(hostObjectID)
-                    })
-                    newCard.appendChild(newHost)
-                }
-            }
+            // if (eventObject.hosts) {
+            //     for (const hostObjectID of eventObject.hosts) {
+            //         const newHost = document.createElement('div');
+            //         newHost.classList.add('show-host')
+            //         const comicObject = comicObjectArray.find(obj => obj._id.toString() === hostObjectID);
+            //         newHost.innerText = `Host: ${comicObject.name}`
+            //         newHost.addEventListener('click', function(event) {
+            //         // console.log(hostObjectID)
+            //         linkToComic(hostObjectID)
+            //         })
+            //         newCard.appendChild(newHost)
+            //     }
+            // }
     
-            if (eventObject.venue) {
-                const newVenue = document.createElement('div');
-                newVenue.classList.add('show-venue')
-                const venueObject = venueObjectArray.find(obj => obj._id.toString() === eventObject.venue);
-                newVenue.innerText = `Venue: ${venueObject.name}`
-                newVenue.addEventListener('click', function(event) {
-                    // console.log(hostObjectID)
-                    linkToVenue(eventObject.venue)
-                    })
-                newCard.appendChild(newVenue)
-            }
+            // if (eventObject.venue) {
+            //     const newVenue = document.createElement('div');
+            //     newVenue.classList.add('show-venue')
+            //     const venueObject = venueObjectArray.find(obj => obj._id.toString() === eventObject.venue);
+            //     newVenue.innerText = `Venue: ${venueObject.name}`
+            //     newVenue.addEventListener('click', function(event) {
+            //         // console.log(hostObjectID)
+            //         linkToVenue(eventObject.venue)
+            //         })
+            //     newCard.appendChild(newVenue)
+            // }
 
             newCard.addEventListener('click', (event) => {
                 expand(eventObject)
@@ -263,28 +263,116 @@ function hideInfo(day) {
 }
 
 function expand(eventObject) {
+    if (document.querySelector('.expanded-card')) {
+        const oldSection = document.querySelector('.expanded-card')
+        body.removeChild(oldSection)
+
+    }
     const newSection = document.createElement('div');
     newSection.classList.add('expanded-card')
     newSection.innerText = `Venue: ${eventObject.name}`
     body.appendChild(newSection)
 
+    
+        if (eventObject.logo) {
+            const newImage = document.createElement('img');
+            newImage.classList.add('show-logo')
+            // console.log(eventObject.logo)
+            // newImage.setAttribute('src',eventObject.logo)
+            newSection.appendChild(newImage)
+        }
+
+        const newName = document.createElement('div');
+        // newName.classList.add('show-name')
+        newName.innerText = eventObject.name
+        newSection.appendChild(newName)
+
+        const newType = document.createElement('div');
+        // newType.classList.add('show-type')
+        newType.innerText = eventObject.type
+        newSection.appendChild(newType)
+
+        const newLocation = document.createElement('div');
+        // newLocation.classList.add('show-location')
+        newLocation.innerText = eventObject.location
+        newSection.appendChild(newLocation)
+
+        const newTime = document.createElement('div');
+        // newTime.classList.add('show-location')
+        newTime.innerText = eventObject.time
+        newSection.appendChild(newTime)
+
+        const newWeekday = document.createElement('div');
+        // newWeekday.classList.add('show-location')
+        newWeekday.innerText = eventObject.weekday
+        newSection.appendChild(newWeekday)
+
+        const newFrequency = document.createElement('div');
+        // newFrequency.classList.add('show-frequency')
+        newFrequency.innerText = eventObject.frequency
+        newSection.appendChild(newFrequency)
+
+        if (eventObject.details) {
+            const newDetails = document.createElement('div');
+            // newDetails.classList.add('show-details')
+            newDetails.innerText = eventObject.details
+            newSection.appendChild(newDetails)
+        }
+
+        if (eventObject.link) {
+            const newLink = document.createElement('div');
+            // newLink.classList.add('show-link')
+            newLink.innerText = eventObject.link
+            newSection.appendChild(newLink)
+        }
+
+        if (eventObject.hosts) {
+            for (const hostObjectID of eventObject.hosts) {
+                const newHost = document.createElement('div');
+                newHost.classList.add('show-host')
+                const comicObject = comicObjectArray.find(obj => obj._id.toString() === hostObjectID);
+                newHost.innerText = `Host: ${comicObject.name}`
+                newHost.addEventListener('click', function(event) {
+                // console.log(hostObjectID)
+                linkToComic(hostObjectID)
+                })
+                newSection.appendChild(newHost)
+            }
+        }
+
+        if (eventObject.venue) {
+            const newVenue = document.createElement('div');
+            newVenue.classList.add('show-venue')
+            const venueObject = venueObjectArray.find(obj => obj._id.toString() === eventObject.venue);
+            newVenue.innerText = `Venue: ${venueObject.name}`
+            newVenue.addEventListener('click', function(event) {
+                // console.log(hostObjectID)
+                linkToVenue(eventObject.venue)
+                })
+                newSection.appendChild(newVenue)
+        }
+    
+
     const newButton = document.createElement('div');
     newButton.innerText = 'Close'
+    newButton.classList.add('button')
     newButton.addEventListener('click', (event) => {
         body.removeChild(newSection)
     })
 
+    const newUpdateButton = document.createElement('div');
+    newUpdateButton.innerText = 'Change Details'
+    newUpdateButton.classList.add('button')
+    newUpdateButton.addEventListener('click', (event) => {
+        displayUpdateForm(newSection,eventObject)
+    })
+
     const newDeleteButton = document.createElement('div');
     newDeleteButton.innerText = 'Delete Event'
+    newDeleteButton.classList.add('button')
     newDeleteButton.addEventListener('click', (event) => {
         body.removeChild(newSection)
         deleteEvent(eventObject)
-    })
-
-    const newUpdateButton = document.createElement('div');
-    newUpdateButton.innerText = 'Change Details'
-    newUpdateButton.addEventListener('click', (event) => {
-        displayUpdateForm(newSection,eventObject)
     })
 
     newSection.appendChild(newButton)
